39ec32a3f3b3fc09907b7c5bb78bc63c70c4bc05
huytoan <huytoan08012000@gmail.com>
2025-08-07 14:24:54 +0700

    edit upload image

--- api/routes/upload-image.js
+++ api/routes/upload-image.js
@@ -3,7 +3,13 @@
 
 
 import { Hono } from 'hono';
-import { v4 as uuidv4 } from 'uuid';
+
+// Hàm tạo UUID v4 thuần JS cho môi trường Cloudflare Workers
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
 
 const uploadImageRouter = new Hono();
 
